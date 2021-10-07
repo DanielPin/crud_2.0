7defea8135393f113273eb97ae196082726c713d
Daniel <daniel.pin@outlook.com>
2021-10-06 22:50:54 -0300

    Configurando GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppService } from './app.service';
 import { ClienteModule } from './cliente/cliente.module';
 
-
 @Module({
-  imports: [TypeOrmModule.forRoot(), ClienteModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    ClienteModule,
+  ],
   controllers: [],
   providers: [AppService],
 })
